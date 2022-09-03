335fd30a49448337e81c4c9f845baad65731525f
arifjahan88 <arifjahan88@gmail.com>
2022-09-03 12:11:53 +0600

    start card body

--- js/app.js
+++ js/app.js
@@ -8,47 +8,61 @@ const loadcatagories = async() =>{
 const displaycatagories = catagories =>{
     const catagorysection = document.getElementById('catagory-section');
     catagories.forEach(catagory => {
-        console.log(catagory);
+        // console.log(catagory);
         const catagorypera = document.createElement('a');
         catagorypera.innerHTML = `
-            <a class="navbar-brand fw-bold" href="#">${catagory.category_name}</a>
+            <a onclick="Loadnewsdetails('${catagory.category_id}')" class="navbar-brand fw-bold" href="#">${catagory.category_name}</a>
         `
         catagorysection.appendChild(catagorypera);
     });
 }
 
-const loadcatagorydetails = async() =>{
-    const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displaycatagorydetails(data.data[0]);
+const Loadnewsdetails = (cat_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${cat_id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaycatagorydetails(data.data))
+    // console.log(url);
 }
 
 const displaycatagorydetails = catagories =>{
     const detailscontainer = document.getElementById('details-container')
-    const catagorydiv = document.createElement('div');
-    catagorydiv.classList.add('row','g-0');
-    catagorydiv.innerHTML = `
+    detailscontainer.textContent = '';
+
+    catagories.forEach(catagory => {
+        console.log(catagory);
+        const catagorydiv = document.createElement('div');
+        catagorydiv.classList.add('row','g-2');
+        catagorydiv.innerHTML = `
     <div class="col-md-4 h-100">
-    <img src="${catagories.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
-  </div>
-  <div class="col-md-8">
-    <div class="card-body">
-      <h5 class="card-title">${catagories.title}</h5>
-      <p class="card-text">
-        ${catagories.details}
-      </p>
-      <p class="card-text">
-        <small class="text-muted pe-5">Auther : ${catagories.author.name}</small>
-        <small class="text-muted">Rating : ${catagories.rating.number}</small>
-      </p>
+        <img src="${catagory.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
     </div>
-  </div>
-    `
+    <div class="col-md-8">
+    <div class="card-body">
+        <h5 class="card-title">${catagory.title}</h5>
+        <p class="card-text">
+            ${catagory.details}
+        </p>
+        <p class="card-text">
+            <small class="text-muted pe-5">Auther : ${catagory.author.name}</small>
+            <small class="text-muted">Rating : ${catagory.rating.number}</small>
+        </p>
+        </div>
+    </div>
+        `
     detailscontainer.appendChild(catagorydiv);
+    })
+
     console.log(catagories);
 }
 
+// const loadcatagorydetails = async() =>{
+//     const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaycatagorydetails(data.data[0]);
+// }
+
 
 loadcatagories();
-loadcatagorydetails();
+//loadcatagorydetails();
